ad7d29c7ef2cdee3f9e51f2c1b5b44c034079e17
Márty <986532741@seznam.cz>
2020-02-10 16:53:19 +0100

    update GMSH_action
    removed useless comments
    added tests

--- src/visip/action/GMSH_action.py
+++ src/visip/action/GMSH_action.py
@@ -15,10 +15,6 @@ class Point:
     x: float = 0.0
     y: float = 0.0
     z: float = 0.0
-    # def __init__(self, x, y, z):
-    #     self._x = x
-    #     self._y = y
-    #     self._z = z
 
 
 @Class
@@ -32,18 +28,6 @@ class Element:
 
     # tags: [region_id, shape_id, partition_id]
 
-    # def __init__(self, type_id: int, dim: int, tags: List[int], nodes: List[int]):
-    #     self._type = type_id
-    #     self._dim = dim
-    #     if len(tags) == 3:
-    #         self._region_id = tags[0]
-    #         self._shape_id = tags[1]
-    #         self._partition_id = tags[2]
-    #     elif len(tags) == 2:
-    #         self._region_id = tags[0]
-    #         self._shape_id = tags[1]
-    #     self._nodes = nodes
-
 
 @Class
 class MeshGMSH:
@@ -52,21 +36,10 @@ class MeshGMSH:
     # regions: Dict[str, List[int]] = None
 
 
-# def __init__(self, nodes: Dict[int, Point], elements: Dict[int, Element], regions: Dict[str, Tuple[int, int]]):
-#     self._nodes = nodes
-#     self._elements = elements
-#     self._regions = regions
-
-
 @action_def
-def GMSH_reader(path):
+def GMSH_reader(path: str) -> MeshGMSH:
     reader = gmsh_io.GmshIO(path)
     reader.read()
-    # print(reader.physical)
-    # print(reader.elements)
-    # print(reader.nodes)
-    # print(reader.element_data)
-    # print(reader.filename)
 
     points = {}
     for idx, point in reader.nodes.items():
@@ -74,8 +47,6 @@ def GMSH_reader(path):
 
     elements = {}
     for idx, element in reader.elements.items():
-        # print(element)
-        # elements[idx] = Element(type_id=element[0], dim=None, tags=element[1], nodes=element[2])
         try:
             elements[idx] = Element(type_id=element[0], dim=None, region_id=element[1][0], shape_id=element[1][1],
                                     partition_id=element[1][2], nodes=element[2])
@@ -84,11 +55,4 @@ def GMSH_reader(path):
                                     nodes=element[2])
 
     my_Mesh = MeshGMSH(points, elements)  # , reader.physical)
-    # print(my_Mesh)
     return my_Mesh
-    # return elements
-
-
-# path = "D:\\Git\\muj_PyBS\\PyBS\\tests\\gmsh\\complex\\meshes\\random_fractures_01.msh"
-# vysledek = GMSH_reader(path)
-# print('_')
--- testing/action/test_GMSH_action.py
+++ testing/action/test_GMSH_action.py
@@ -19,6 +19,12 @@ def test_GMSH_Point():
     assert params[0].name == 'x'
     assert params[1].name == 'y'
     assert params[2].name == 'z'
+    point_xyz = wrap.public_action(ClassActionBase.construct_from_params("PointXYZ", params))
+    xyz_instance = point_xyz(x=0, y=11, z=222)._action_call
+    assert xyz_instance
+    assert xyz_instance.arguments[0].value.action.value == 0
+    assert xyz_instance.arguments[1].value.action.value == 11
+    assert xyz_instance.arguments[2].value.action.value == 222
 
 
 def test_GMSH_Element():
@@ -31,6 +37,35 @@ def test_GMSH_Element():
     assert params[3].name == 'shape_id'
     assert params[4].name == 'partition_id' and params[4].type == int
     assert params[5].name == 'nodes' and params[5].type == typing.List[int]
+    element_test = wrap.public_action(ClassActionBase.construct_from_params('element_test', params))
+    element_instance = element_test(type_id=1, dim=2, region_id=3, shape_id=4, partition_id=5,
+                                    nodes=[1, 2, 3])._action_call
+    assert element_instance
+    assert element_instance.arguments[0].value.action.value == 1
+    assert element_instance.arguments[1].value.action.value == 2
+    assert element_instance.arguments[5].value.arguments[0].value.action.value == 1
+    assert element_instance.arguments[5].value.arguments[1].value.action.value == 2
+    assert element_instance.arguments[5].value.arguments[2].value.action.value == 3
+
+
+def prepare_nodes():
+    point_wrap = Point
+    point_action = point_wrap.action
+    params = list(point_action.parameters)
+    point_xyz = wrap.public_action(ClassActionBase.construct_from_params("PointXYZ", params))
+    xyz_instance = point_xyz(x=0, y=11, z=222)._action_call
+    return {0: xyz_instance}
+
+
+def prepare_elements():
+    element_wrap = Element
+    element_action = element_wrap.action
+    params = list(element_action.parameters)
+    element_test = wrap.public_action(ClassActionBase.construct_from_params('element_test', params))
+    element_instance = element_test(type_id=1, dim=2, region_id=3, shape_id=4, partition_id=5,
+                                    nodes=[1, 2, 3])._action_call
+
+    return {0: element_instance}
 
 
 def test_GMSH_Mesh():
@@ -38,9 +73,10 @@ def test_GMSH_Mesh():
     mesh_action = mesh_wrap.action
     params = list(mesh_action.parameters)
     assert params
-    print(params)
+    mesh_test = wrap.public_action(ClassActionBase.construct_from_params('mesh_test', params))
+    mesh_instance = mesh_test(nodes=prepare_nodes(), elements=prepare_elements())._action_call
+    assert mesh_instance
+    assert mesh_instance.parameters.parameters[0].name == 'nodes'
+    assert mesh_instance.parameters.parameters[1].name == 'elements'
 
-# @pytest.mark.parametrize('file', ['D:\\Git\\muj_PyBS\\PyBS\\tests\\gmsh\\complex\\meshes\\random_fractures_01.msh'])
-# def test_GMSH_reader(file):
-#     reader = GMSH_reader(file).action
-#     print(reader)
+
