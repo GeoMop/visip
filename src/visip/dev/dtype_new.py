6ad0a38878c3b58085d09a96ed13c71ec4bc13cd
Radek Srb <radeksrb@email.cz>
2021-10-20 12:53:57 +0200

    types - is_subtype

--- src/visip/dev/dtype_new.py
+++ src/visip/dev/dtype_new.py
@@ -21,9 +21,10 @@ class Str:
 
 
 class Class:
-    def __init__(self, module, name):
+    def __init__(self, module, name, origin_type):
         self.module = module
         self.name = name
+        self.origin_type = origin_type
 
 
 class List:
@@ -47,59 +48,186 @@ class Union:
         self.args = args
 
 
-class VarType:
+class Const:
+    def __init__(self, arg):
+        self.arg = arg
+
+
+class TypeVar:
     def __init__(self, name):
         self.name = name
 
 
 def from_typing(type):
+    t, _ = from_typing_map(type, {})
+    return t
+
+
+def from_typing_map(type, var_map):
+    print(type)
     # base
     if type is int:
-        return Int()
+        return Int(), var_map
 
     if type is float:
-        return Float()
+        return Float(), var_map
 
     if type is bool:
-        return Bool()
+        return Bool(), var_map
 
     if type is str:
-        return Str()
+        return Str(), var_map
 
 
-    # VarType
+    # TypeVar
     if typing_inspect.is_typevar(type):
-        return VarType(type.__name__)
+        if type in var_map:
+            return var_map[type], var_map
+        t = TypeVar(type.__name__)
+        var_map = var_map.copy()
+        var_map[type] = t
+        return t, var_map
 
 
     # Tuple
     if typing_inspect.is_tuple_type(type):
-        args = typing_inspect.get_args(type)
-        return Tuple([from_typing(a) for a in args])
+        args = []
+        xx = typing_inspect.get_args(type, evaluate=True)
+        for a in typing_inspect.get_args(type, evaluate=True):
+            t, var_map = from_typing_map(a, var_map)
+            args.append(t)
+        return Tuple(args), var_map
 
 
     # Union
     if typing_inspect.is_union_type(type):
-        args = typing_inspect.get_args(type)
-        return Union([from_typing(a) for a in args])
+        args = []
+        for a in typing_inspect.get_args(type, evaluate=True):
+            t, var_map = from_typing_map(a, var_map)
+            args.append(t)
+        return Union(args), var_map
 
 
     origin = typing_inspect.get_origin(type)
 
     # List
     if origin in [list, typing.List]:
-        return List(from_typing(typing_inspect.get_args(type)[0]))
+        t, var_map = from_typing_map(typing_inspect.get_args(type, evaluate=True)[0], var_map)
+        return List(t), var_map
 
     # Dict
     if origin in [dict, typing.Dict]:
-        args = typing_inspect.get_args(type)
-        return Dict(from_typing(args[0]), from_typing(args[1]))
+        args = typing_inspect.get_args(type, evaluate=True)
+        k, var_map = from_typing_map(args[0], var_map)
+        v, var_map = from_typing_map(args[1], var_map)
+        return Dict(k, v), var_map
+
+    # Const
+    if origin is dtype.Constant:
+        t, var_map = from_typing_map(typing_inspect.get_args(type, evaluate=True)[0], var_map)
+        return Const(t), var_map
 
 
     # Class
     if issubclass(type, dtype.DataClassBase):
-        return Class(type.__module__, type.__name__)
+        return Class(type.__module__, type.__name__, type), var_map
 
 
-def is_subtype(a, b):
-    return False
+    raise TypeError("Not supported type.")
+
+
+def is_subtype(type, typeinfo):
+    b, _ = is_subtype_map(type, typeinfo, {})
+    return b
+
+
+def is_subtype_map(type, typeinfo, var_map, const=False):
+    # var_map is not yet fully implemented
+
+    # if type is Const, call recursively with const=True
+    if isinstance(type, Const):
+        return is_subtype_map(type.arg, typeinfo, var_map, True)
+
+    # if typeinfo is Const, check if const==True then call recursively
+    elif isinstance(typeinfo, Const):
+        if const:
+            return is_subtype_map(type, typeinfo.arg, var_map, True)
+
+    # if typeinfo is TypeVar, always True
+    elif isinstance(typeinfo, TypeVar):
+        return True, []
+
+    # if type is Union, must be satisfied for all args
+    elif isinstance(type, Union):
+        vm_list = []
+        for arg in type.args:
+            b, vm = is_subtype_map(arg, typeinfo, var_map, const)
+            if not b:
+                return False, []
+            vm_list = _vm_merge(vm_list, vm)
+        return True, vm_list
+
+    # if typeinfo is Union, must be satisfied for at least one arg
+    elif isinstance(typeinfo, Union):
+        for arg in typeinfo.args:
+            b, vm = is_subtype_map(type, arg, var_map, const)
+            if b:
+                return True, []
+
+    # if type is Int, typeinfo must be Int
+    elif isinstance(type, Int):
+        if isinstance(typeinfo, Int):
+            return True, [var_map]
+
+    # if type is Float, typeinfo must be Float
+    elif isinstance(type, Float):
+        if isinstance(typeinfo, Float):
+            return True, [var_map]
+
+    # if type is Bool, typeinfo must be Bool or Int
+    elif isinstance(type, Bool):
+        if isinstance(typeinfo, (Bool, Int)):
+            return True, [var_map]
+
+    # if type is Str, typeinfo must be Str
+    elif isinstance(type, Str):
+        if isinstance(typeinfo, Str):
+            return True, [var_map]
+
+    # if type is Class, typeinfo must be Class and type.origin_type must be subclass of typeinfo.origin_type
+    elif isinstance(type, Class):
+        if isinstance(typeinfo, Class) and issubclass(type.origin_type, typeinfo.origin_type):
+            return True, [var_map]
+
+    # if type is Tuple, typeinfo must be Tuple and all args must be subtype
+    elif isinstance(type, Tuple):
+        if isinstance(typeinfo, Tuple) and len(type.args) == len(typeinfo.args):
+            vm_list = [var_map]
+            for arg, arginfo in zip(type.args, typeinfo.args):
+                new_vm_list = []
+                b, vm = is_subtype_map(arg, arginfo, var_map, const)
+                if not b:
+                    return False, []
+            return True, vm_list
+
+    # if type is List, typeinfo must be List and args must be subtype
+    elif isinstance(type, List):
+        if isinstance(typeinfo, List):
+            b, vm = is_subtype_map(type.arg, typeinfo.arg, var_map, const)
+            if b:
+                return True, [var_map]
+
+    # if type is Dict, typeinfo must be Dict and keys, values must be subtype
+    elif isinstance(type, Dict):
+        if isinstance(typeinfo, Dict):
+            b, vm = is_subtype_map(type.key, typeinfo.key, var_map, const)
+            if b:
+                b, vm = is_subtype_map(type.value, typeinfo.value, var_map, const)
+                if b:
+                    return True, [var_map]
+
+    return False, []
+
+
+def _vm_merge(vm1, vm2):
+    return vm1 + vm2
--- testing/dev/test_dtype_new.py
+++ testing/dev/test_dtype_new.py
@@ -11,11 +11,16 @@ def test_from_typing():
     assert isinstance(dtype_new.from_typing(str), dtype_new.Str)
 
 
-    # VarType
+    # TypeVar
     t = typing.TypeVar("T")
     nt = dtype_new.from_typing(t)
-    assert isinstance(nt, dtype_new.VarType)
+    assert isinstance(nt, dtype_new.TypeVar)
     assert nt.name == t.__name__
+
+    tt = typing.Tuple[t, typing.List[t]]
+    print(tt)
+    nt = dtype_new.from_typing(tt)
+    assert nt.args[0] is nt.args[1].arg
 
 
     # Tuple
@@ -38,7 +43,6 @@ def test_from_typing():
     assert isinstance(nt, dtype_new.List)
     assert isinstance(nt.arg, dtype_new.Int)
 
-
     # Dict
     t = typing.Dict[int, str]
     nt = dtype_new.from_typing(t)
@@ -46,11 +50,16 @@ def test_from_typing():
     assert isinstance(nt.key, dtype_new.Int)
     assert isinstance(nt.value, dtype_new.Str)
 
+    # Const
+    t = dtype.Constant[int]
+    nt = dtype_new.from_typing(t)
+    assert isinstance(nt, dtype_new.Const)
+    assert isinstance(nt.arg, dtype_new.Int)
+
 
     # Class
     class A(dtype.DataClassBase):
         pass
-
 
     nt = dtype_new.from_typing(A)
     assert isinstance(nt, dtype_new.Class)
@@ -59,4 +68,79 @@ def test_from_typing():
 
 
 def test_is_subtype():
-    assert not dtype_new.is_subtype(dtype_new.Int(), dtype_new.Int())
+    dt = dtype_new
+    sub = dt.is_subtype
+    
+    # base
+    assert sub(dt.Int(), dt.Int())
+    assert not sub(dt.Int(), dt.Str())
+
+    assert sub(dt.Str(), dt.Str())
+    assert sub(dt.Float(), dt.Float())
+    assert sub(dt.Bool(), dt.Bool())
+
+    assert sub(dt.Bool(), dt.Int())
+    assert not sub(dt.Int(), dt.Bool())
+
+    # Class
+    class A(dtype.DataClassBase):
+        pass
+
+    class B(dtype.DataClassBase):
+        pass
+
+    class C(A):
+        pass
+
+    a = dtype_new.from_typing(A)
+    a2 = dtype_new.from_typing(A)
+    b = dtype_new.from_typing(B)
+    c = dtype_new.from_typing(C)
+
+    assert sub(a, a2)
+    assert sub(a2, a)
+    assert not sub(a, b)
+    assert sub(c, a)
+    assert not sub(a, c)
+
+    # Union
+    assert sub(dt.Int(), dt.Union([dt.Int(), dt.Str()]))
+    assert not sub(dt.Union([dt.Int(), dt.Str()]), dt.Int())
+    assert sub(dt.Union([dt.Int(), dt.Str()]), dt.Union([dt.Int(), dt.Str()]))
+    assert sub(dt.Union([dt.Union([dt.Int()]), dt.Str()]), dt.Union([dt.Int(), dt.Str()]))
+    assert sub(dt.Union([dt.Int(), dt.Str()]), dt.Union([dt.Union([dt.Int()]), dt.Str()]))
+
+    # List
+    assert sub(dt.List(dt.Int()), dt.List(dt.Int()))
+    assert not sub(dt.List(dt.Int()), dt.List(dt.Str()))
+
+    # Dict
+    assert sub(dt.Dict(dt.Int(), dt.Str()), dt.Dict(dt.Int(), dt.Str()))
+    assert not sub(dt.Dict(dt.Int(), dt.Str()), dt.Dict(dt.Str(), dt.Str()))
+    assert not sub(dt.Dict(dt.Int(), dt.Str()), dt.Dict(dt.Int(), dt.Int()))
+
+    # Tuple
+    assert sub(dt.Tuple([dt.Int(), dt.Str()]), dt.Tuple([dt.Int(), dt.Str()]))
+    assert not sub(dt.Tuple([dt.Int(), dt.Str()]), dt.Tuple([dt.Int(), dt.Int()]))
+
+    # Const
+    assert sub(dt.Const(dt.Int()), dt.Int())
+    assert not sub(dt.Int(), dt.Const(dt.Int()))
+    assert sub(dt.Const(dt.Int()), dt.Const(dt.Int()))
+
+    assert sub(dt.Const(dt.Const(dt.Int())), dt.Int())
+    
+    assert sub(dt.Const(dt.Tuple([dt.Int(), dt.Str()])), dt.Tuple([dt.Const(dt.Int()), dt.Str()]))
+
+    # TypeVar
+    t = dt.TypeVar("T")
+    u = dt.TypeVar("U")
+
+    assert sub(dt.Int(), t)
+    assert not sub(t, dt.Int())
+    assert sub(t, u)
+    assert sub(dt.Int(), dt.Union([t, u]))
+    assert sub(dt.Union([dt.Int(), dt.Str()]), t)
+    assert sub(dt.Union([dt.Int(), dt.Str()]), dt.Union([t, u]))
+    assert sub(dt.Tuple([dt.Union([dt.Int(), dt.Str()]), dt.Int()]), dt.Tuple([t, t]))
+    assert sub(dt.Tuple([dt.Union([dt.Int(), dt.Str()]), dt.Int()]), dt.Tuple([dt.Union([t, dt.Str()]), t]))
