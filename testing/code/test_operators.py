b15920a874b73b17354b367beb01f7f8151efd7d
Jan Brezina <jan.brezina@tul.cz>
2022-04-23 16:48:13 +0200

    Fix tests

--- testing/code/test_operators.py
+++ testing/code/test_operators.py
@@ -3,23 +3,23 @@ from visip.dev import evaluation
 
 @wf.workflow
 def arithmetic_ops(self, a, b):
-    self.a = a + b
-    self.b = a - b
+    self.A = a + b
+    self.B = a - b
     self.c = a * b
     self.d = a / b
     self.e = b % 1
     self.f = a ** 3
-    return [self.a, self.b, self.c, self.d, self.e, self.f]
+    return [self.A, self.B, self.c, self.d, self.e, self.f]
 
 @wf.workflow
 def comparison_ops(self, a, b):
-    self.a = a > 0
-    self.b = a == 1
+    self.A = a > 0
+    self.B = a == 1
     self.c = a >= 2
     self.d = b < 2
     self.e = b <= 2
     self.f = b != 2
-    return [self.a, self.b, self.c, self.d, self.e, self.f]
+    return [self.A, self.B, self.c, self.d, self.e, self.f]
 
 def test_operators():
     result = evaluation.run(arithmetic_ops, 1, 2)
